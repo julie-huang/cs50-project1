cec78b710a1afc1ec1292c30fd2523ab91c4e4f5
julieh <[julie.huang@alumni.ubc.ca]>
2019-02-18 21:44:34 -0800

    added search results

--- application.py
+++ application.py
@@ -44,10 +44,38 @@ def login():
             return render_template("error.html", message=error)
         else:
             session['username']=True
-            return render_template("home.html")
+            return render_template("search.html")
 
     return render_template("login.html")
 
-@app.route("/home")
-def home():
-    return render_template("home.html")
+
+@app.route("/search", methods=['GET', 'POST'])
+def search(): 
+    error=""
+    if request.method =='POST':
+        q = request.form['q']
+        search_query = '%' + q + '%'
+        data = db.execute("SELECT * FROM books WHERE title ILIKE (:q) OR isbn ILIKE (:q) or author ILIKE (:q)", {'q': search_query}).fetchall()
+
+        #  see if the data is retrieved 
+        for d in data: 
+            print(f"{d.title}")
+        
+        if data is None: 
+            error="No matches found!"
+            return render_template("error.html", message=error)
+        else: 
+            return render_template("result.html", results=data)
+
+    return render_template("search.html", results=data)
+
+@app.route("/result")
+def result():
+    return render_template("result.html")
+    
+
+@app.route("/books/<string:isbn>", methods=['GET', 'POST'])
+def books(isbn):
+    data = db.execute("SELECT * FROM books WHERE isbn = (:isbn)",
+            {"isbn": isbn}).fetchall()
+    return render_template("books.html", book=data[0])
