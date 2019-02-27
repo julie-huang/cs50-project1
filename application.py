0fa0bf02c82ce982b9a0c1e66865c586bddeaa97
julieh <[julie.huang@alumni.ubc.ca]>
2019-02-26 22:14:47 -0800

    implemented api

--- application.py
+++ application.py
@@ -1,13 +1,14 @@
 import os
 import requests
 
-from flask import Flask, session, render_template, request, redirect
+from flask import Flask, session, render_template, request, redirect, jsonify
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
@@ -22,61 +23,103 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/register")
+
+@app.route("/register", methods=['POST', 'GET'])
 def register():
-    return render_template("register.html")
-
-@app.route("/login", methods=['GET','POST'])
-def login():
-    error=""
-    # get form info
-    if request.method =='POST':
-        username=request.form['username']
-        password=request.form['password']
-        user=db.execute("SELECT * FROM users WHERE username=:username and password=:password",{"username":username,"password":password}).fetchone()
-
-        if user is None:
-            error="Invalid username and or password, please try again"
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        email = request.form.get('email')
+        user = db.execute("SELECT username, password FROM users WHERE username=:username and password=:password",
+                          {"username": username, "password": password}).fetchall()
+        if user != []:
+            error = "Username has already been taken. Please choose another one."
             return render_template("error.html", message=error)
         else:
-            session['username']=True
+            db.execute(('INSERT INTO users (username, password, email)'
+                        'VALUES (:username, :password, :email)'),
+                       {"username": username, "password": password, "email": email})
+            db.commit()
+            session["username"] = True
+            return render_template("search.html")
+    return render_template("register.html")
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    error = ""
+    # get form info
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = db.execute("SELECT * FROM users WHERE username=:username and password=:password",
+                          {"username": username, "password": password}).fetchone()
+
+        if user is None:
+            error = "Invalid username and or password, please try again"
+            return render_template("error.html", message=error)
+        else:
+            session['username'] = True
             return render_template("search.html")
 
     return render_template("login.html")
 
 
 @app.route("/search", methods=['GET', 'POST'])
-def search(): 
-    error=""
-    name=request.form.get('username')
-    if request.method =='POST':
+def search():
+    error = ""
+    name = request.form.get('username')
+    # if we are putting a search
+    if request.method == 'POST':
         q = request.form['q']
         search_query = '%' + q + '%'
-        data = db.execute("SELECT * FROM books WHERE title ILIKE (:q) OR isbn ILIKE (:q) or author ILIKE (:q)", {'q': search_query}).fetchall()
+        data = db.execute(
+            "SELECT * FROM books WHERE title ILIKE (:q) OR isbn ILIKE (:q) or author ILIKE (:q)", {'q': search_query}).fetchall()
 
-        #  see if the data is retrieved 
-        for d in data: 
+        #  see if the data is retrieved
+        for d in data:
             print(f"{d.title}")
-        
-        if data is None: 
-            error="No matches found!"
+
+        if data is None:
+            error = "No matches found!"
             return render_template("error.html", message=error)
-        else: 
+        else:
             return render_template("result.html", results=data)
 
     return render_template("search.html", results=data, name=name)
 
+
 @app.route("/result")
 def result():
     return render_template("result.html")
-    
+
 
 @app.route("/books/<string:isbn>", methods=['GET', 'POST'])
 def books(isbn):
-    data = db.execute("SELECT * FROM books WHERE isbn = (:isbn)",
-            {"isbn": isbn}).fetchall()
+    data = db.execute("SELECT * FROM books WHERE isbn=:isbn",
+                      {"isbn": isbn}).fetchall()
     return render_template("books.html", book=data[0])
+
+
+@app.route("/api/book/<string:isbn>", methods=['GET'])
+def books_api(isbn):
+
+      # Make sure flight exists.
+    book = db.execute("SELECT * FROM books where isbn=:isbn",
+                      {"isbn": isbn}).fetchone()
+    if book is None:
+        return jsonify({"error": "Invalid isbn"}), 404
+
+    return jsonify({
+        "title": book.title,
+        "author": book.author,
+        "year": book.year,
+        "isbn": book.isbn
+        # "review_count": db.execute("SELECT review_count from books where isbn=:isbn {"isbn": isbn}).fetchall()
+        # "average_score": db.execute("SELECT title from books where isbn=:isbn {"isbn": isbn}).fetchall()
+    })
--- import.py
+++ import.py
@@ -1,5 +1,5 @@
-import os 
-import csv 
+import os
+import csv
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -8,18 +8,18 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 engine = create_engine(os.getenv('DATABASE_URL'))
 db = scoped_session(sessionmaker(bind=engine))
 
-def main(): 
-    with open('books.csv', newline='') as f: 
-        reader=csv.reader(f, delimiter=',')
+
+def main():
+    with open('books.csv', newline='') as f:
+        reader = csv.reader(f, delimiter=',')
         next(reader)
         for isbn, title, author, year in reader:
             db.execute(('INSERT INTO books (isbn, title, author, year)'
-            ' VALUES (:isbn, :title, :author, :year)'), 
-            {'isbn': isbn, 'title': title, 'author': author, 'year': int(year)})
+                        ' VALUES (:isbn, :title, :author, :year)'),
+                       {'isbn': isbn, 'title': title, 'author': author, 'year': int(year)})
             print(title)
         db.commit()
 
 
 if __name__ == "__main__":
     main()
-
